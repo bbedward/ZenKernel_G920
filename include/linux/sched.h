701d35cf78659db1bcb8312797c052479d34e0ff
Brandon Berhent <bbedward@gmail.com>
2015-04-05 14:17:14 -0400

    bfs: implement wake_up_if_idle

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -894,6 +894,8 @@ void free_sched_domains(cpumask_var_t doms[], unsigned int ndoms);
 
 bool cpus_share_cache(int this_cpu, int that_cpu);
 
+extern void wake_up_if_idle(int cpu);
+
 #ifdef CONFIG_SCHED_HMP
 struct hmp_domain {
 	struct cpumask cpus;
